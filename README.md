3aa1ea7ad556058c392635ae9cded4c757940e78
Beasta <barryblaha@gmail.com>
2017-05-18 13:28:12 -0700

    First commit for grump init!

---
+++ README.md
@@ -0,0 +1,14 @@
+# init
+This is a script to quickly initialize a grump.
+
+If grump has not yet been installed, you can do so using:
+```bash
+npm i -g grump
+```
+
+Example usage:
+```
+grump init
+```
+
+see [grumpJS.com](https://grumpjs.com) to find other useful grumps!
---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "what will the grump be called"
+read grumpName
+echo "creating a grump.json"
+cat >grump.json <<EOL
+
+{
+  "defaultCommand": "${grumpName}",
+  "commands": {
+    "${grumpName}": {
+      "scriptType": "bash",
+      "scriptPath": ${grumpName}.sh",
+      "vars": {}
+    }
+  }
+}
+EOL
+echo "creating ${grumpName} shell file"
+echo "#!/bin/bash" > ${grumpName}.sh
+git int
